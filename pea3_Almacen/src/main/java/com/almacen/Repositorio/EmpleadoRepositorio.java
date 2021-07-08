8ee255d8a6d39ea9f4df0e5f6827cd6425942ae9
Johan8AxD <86860229+Johan8AxD@users.noreply.github.com>
2021-07-08 10:22:47 -0500

    Delete pea3_Almacen directory

--- pea3_Almacen/src/main/java/com/almacen/Repositorio/EmpleadoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.almacen.Repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.almacen.Entidad.Empleado;
-
-public interface EmpleadoRepositorio extends JpaRepository<Empleado, Integer>{
-
-}
--- pea3_Almacen/src/main/java/com/almacen/Repositorio/PedidoInternoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.almacen.Repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.almacen.Entidad.PedidoInterno;
-
-public interface PedidoInternoRepositorio extends JpaRepository<PedidoInterno, Integer>{
-
-}

c3734626ea2645a79571cadcc8a933544b825ac6
Johan8AxD <86860229+Johan8AxD@users.noreply.github.com>
2021-07-07 23:05:18 -0500

    Proyecto de Mcro-Servicios

---
+++ ms_project_configservice/src/main/java/com/proyec/web/ms/MsProjectConfigserviceApplication.java
@@ -0,0 +1,14 @@
+package com.proyec.web.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MsProjectConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProjectConfigserviceApplication.class, args);
+	}
+
+}
